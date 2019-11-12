925d6ef269328f37b95c1bd91cf4dda415583060
Gwendal Daniel <gwendal.daniel@gmail.com>
2019-11-12 15:22:49 +0100

    add BotTest

---
+++ runtime/src/test/java/com/xatkit/plugins/react/BotTest.java
@@ -0,0 +1,18 @@
+package com.xatkit.plugins.react;
+
+import com.xatkit.Xatkit;
+
+/**
+ * This class is used to run existing bots, and should not contain test cases.
+ */
+public class BotTest {
+
+    public static void main(String[] args) {
+        Xatkit.main(new String[]{"<Path to your .properties file>"});
+        try {
+            Thread.sleep(10000000);
+        }catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
